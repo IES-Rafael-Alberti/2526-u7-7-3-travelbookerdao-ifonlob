b3b9f845d6330cc6ed0bdbb49b37e6e39831b55b
ifonlob <ifonlob1108@g.educaand.es>
2026-04-16 11:51:56 +0200

    Añadidos métodos de creación de reservas

--- src/main/kotlin/es/iesra/datos/ReservaHotelDAO.kt
+++ src/main/kotlin/es/iesra/datos/ReservaHotelDAO.kt
@@ -5,7 +5,13 @@ import es.iesra.dominio.ReservaVuelo
 import java.io.File
 
 class ReservaHotelDAO(private val file : File) : IDAO<ReservaHotel>{
-    override fun create(entity: ReservaHotel) = file.appendText("${entity.id},${entity.descripcion},${entity.ubicacion},${entity.numeroNoches}")
+    override fun create(entity: ReservaHotel){
+        val entidadParseada = "${entity.id},${entity.descripcion},${entity.ubicacion},${entity.numeroNoches}"
+
+        if (file.readLines().contains(entidadParseada)) throw IllegalArgumentException("La reserva ya existe en el fichero.")
+        else file.appendText(entidadParseada)
+
+    }
 
     override fun read(): List<ReservaHotel> {
         val lineas = file.readLines()
--- src/main/kotlin/es/iesra/datos/ReservaVueloDAO.kt
+++ src/main/kotlin/es/iesra/datos/ReservaVueloDAO.kt
@@ -1,10 +1,21 @@
 package es.iesra.datos
 
+import es.iesra.dominio.ReservaHotel
 import es.iesra.dominio.ReservaVuelo
 import java.io.File
 
 class ReservaVueloDAO(private val file : File): IDAO<ReservaVuelo> {
-    override fun create(entity: ReservaVuelo) = file.appendText("${entity.id},${entity.descripcion},${entity.origen},${entity.destino},${entity.horaVuelo}")
+
+    override fun create(entity: ReservaVuelo){
+        val entidadParseada = "${entity.id},${entity.descripcion},${entity.origen},${entity.destino},${entity.horaVuelo}"
+
+        if (file.readLines().contains(entidadParseada)) throw IllegalArgumentException("La reserva ya existe en el fichero.")
+        else file.appendText(entidadParseada)
+
+    }
+
+
+
 
     override fun read(entity: ReservaVuelo) : List<ReservaVuelo>{
         TODO("Not yet implemented")
